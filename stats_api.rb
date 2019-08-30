9a2169c252024a7276d2da2f82d9e653886f80bf
GeoffedUP <35282584+GeoffedUP@users.noreply.github.com>
2019-08-30 12:54:54 -0700

    Update stats_api.rb

--- stats_api.rb
+++ stats_api.rb
@@ -15,7 +15,7 @@ stats_api_thread = StatsApiThread.new(
 )
 
 get "/stats.json" do
-#  content_type :json
+  content_type :json
   response.headers["Access-Control-Allow-Origin"] = "*"
-  JSON.pretty_generate($stats_json)
+  $stats_json
 end
